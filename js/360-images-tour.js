48d913f8f59a204e9731f1865514669170cd48f0
quarkpott <wurmmehl@gmail.com>
2025-06-30 23:39:19 +0200

    add quick selection to 360 degree images

--- js/360-images-tour.js
+++ js/360-images-tour.js
@@ -1,35 +1,44 @@
 const scenes = {
-  "images/test1.jpg": [
-    {
-      position: "0 0 -5",
-      target: "images/test2.jpg",
-      type: "image",
-    },
-    {
-      position: "1 0 -5",
-      target: "model.html",
-      type: "page",
-    },
-  ],
-  "images/test2.jpg": [
-    {
-      position: "2 1 -5",
-      target: "images/test3.jpg",
-      type: "image",
-    },
-    {
-      position: "-2 0 -4",
-      target: "images/test1.jpg",
-      type: "image",
-    },
-  ],
-  "images/test3.jpg": [
-    {
-      position: "0 1 -6",
-      target: "images/test1.jpg",
-      type: "image",
-    },
-  ],
+  "images/test1.jpg": {
+    hotspots: [
+      {
+        position: "0 0 -5",
+        target: "images/test2.jpg",
+        type: "image",
+      },
+      {
+        position: "1 0 -5",
+        target: "model.html",
+        type: "page",
+      },
+    ],
+    description: "Wasser",
+  },
+  "images/test2.jpg": {
+    hotspots: [
+      {
+        position: "2 1 -5",
+        target: "images/test3.jpg",
+        type: "image",
+      },
+      {
+        position: "-2 0 -4",
+        target: "images/test1.jpg",
+        type: "image",
+      },
+    ],
+    description: "Winter",
+  },
+  "images/test3.jpg": {
+    hotspots: [
+      {
+        position: "0 1 -6",
+        target: "images/test1.jpg",
+        type: "image",
+      },
+    ],
+    description: "Berggipfel",
+  },
 };
 
 // Hotspot-Komponente
@@ -41,12 +50,10 @@ AFRAME.registerComponent("hotspot", {
   init() {
     this.el.addEventListener("click", () => {
       if (this.data.targetType === "image") {
-        // Bildwechsel-Logik
         document.querySelector("a-sky").setAttribute("src", this.data.target);
         this.el.remove();
         createHotspots(this.data.target);
       } else if (this.data.targetType === "page") {
-        // Seitenwechsel-Logik
         window.location.href = this.data.target;
       }
     });
@@ -56,24 +63,39 @@ AFRAME.registerComponent("hotspot", {
 // Hotspots erzeugen
 const createHotspots = (img) => {
   document.querySelectorAll(".hotspot").forEach((e) => e.remove());
-  (scenes[img] || []).forEach(({ position, target, type }) => {
-    const h = document.createElement("a-entity");
-    h.setAttribute("geometry", "primitive: plane; height: 0.6; width: 0.6");
-    h.setAttribute("material", "src: #stern; transparent: true");
-    h.setAttribute("position", position);
-
-    // Setze beide Parameter für die Komponente
-    h.setAttribute("hotspot", {
-      target: target,
-      targetType: type,
+  const sceneData = scenes[img];
+  if (sceneData) {
+    sceneData.hotspots.forEach(({ position, target, type }) => {
+      const h = document.createElement("a-entity");
+      h.setAttribute("geometry", "primitive: plane; height: 0.6; width: 0.6");
+      h.setAttribute("material", "src: #stern; transparent: true");
+      h.setAttribute("position", position);
+      h.setAttribute("hotspot", { target: target, targetType: type });
+      h.setAttribute("class", "hotspot");
+      h.setAttribute("look-at", "[camera]");
+      document.querySelector("a-scene").appendChild(h);
     });
-
-    h.setAttribute("class", "hotspot");
-    h.setAttribute("look-at", "[camera]");
-    document.querySelector("a-scene").appendChild(h);
-  });
+  }
 };
 
-document.addEventListener("DOMContentLoaded", () =>
-  createHotspots("images/test1.jpg")
-);
+// Schnellauswahl erstellen
+function createSceneSelector() {
+  const selector = document.getElementById("scene-selector");
+
+  for (const [img, data] of Object.entries(scenes)) {
+    const button = document.createElement("button");
+    button.className = "scene-button";
+    button.textContent = data.description;
+    button.onclick = () => {
+      // Aktuelle Szene wechseln
+      document.querySelector("a-sky").setAttribute("src", img);
+      createHotspots(img);
+    };
+    selector.appendChild(button);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  createHotspots("images/test1.jpg");
+  createSceneSelector(); // Schnellauswahl erstellen
+});
